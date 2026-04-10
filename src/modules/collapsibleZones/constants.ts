acb468527cbcee94d3cb143d412cb4d9a877f6c6
MarcusIsLion <126798166+MarcusIsLion@users.noreply.github.com>
2026-04-10 19:22:07 +0200

    Fix

--- src/modules/collapsibleZones/constants.ts
+++ src/modules/collapsibleZones/constants.ts
@@ -1,9 +1,10 @@
 export const COLLAPSE_SELECTORS = {
     PAGE_MATCH: 'park/attractions.php',
-    ZONE_GROUP: '.owned-attractions__zone-group',
-    ZONE_HEADER: '.owned-attractions__zone-header',
-    ZONE_CONTENT: '.owned-attractions__zone-attractions',
+    ZONE_GROUP: '.park-attractions-group',
+    ZONE_HEADER: '.dash-block__head',
+    ZONE_CONTENT: '.dash-block__body',
     COLLAPSED_CLASS: 'tpi-zone-collapsed',
+    ENABLED_CLASS: 'tpi-collapsible-enabled',
 };
 
 export const STORAGE_KEYS = {
