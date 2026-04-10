2378fe2190d0b921ae205f9c6bcd0556b346a8d9
MarcusIsLion <126798166+MarcusIsLion@users.noreply.github.com>
2026-04-10 20:17:43 +0200

    format

--- src/modules/collapsibleZones/constants.ts
+++ src/modules/collapsibleZones/constants.ts
@@ -1,6 +1,12 @@
 export const COLLAPSE_SELECTORS = {
-    PAGE_MATCHES: ['park/attractions.php', 'park/spectacles.php', 'park/boutiques.php', 'park/restaurants.php'],
-    ZONE_GROUP: '.park-attractions-group, .park-spectacles-group, .park-boutiques-group, .park-restaurants-group',
+    PAGE_MATCHES: [
+        'park/attractions.php',
+        'park/spectacles.php',
+        'park/boutiques.php',
+        'park/restaurants.php',
+    ],
+    ZONE_GROUP:
+        '.park-attractions-group, .park-spectacles-group, .park-boutiques-group, .park-restaurants-group',
     ZONE_HEADER: '.dash-block__head',
     ZONE_CONTENT: '.dash-block__body',
     COLLAPSED_CLASS: 'tpi-zone-collapsed',
